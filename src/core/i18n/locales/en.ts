db62bea400bec23e9f66e8a280a5d32121db1659
Tobias Prima <primatjahja@gmail.com>
2025-12-02 22:27:53 +0800

    feat: i18n translations

---
+++ src/core/i18n/locales/en.ts
@@ -0,0 +1,27 @@
+export const en = {
+  premium: {
+    backLabel: '<',
+    titleLine1: 'Got more questions?',
+    titleLine2: 'GUM Specialists are here!',
+    description:
+      'We provide professional, impartial advice to help you find what\'s best for you',
+    serviceHoursLabel: 'Service Hours:',
+    serviceHoursWeekdays: 'Monday - Friday: 9:30am - 5:30pm',
+    serviceHoursWeekend: 'Saturday, Sunday & Public Holidays: Closed',
+    bookAppointment: 'Book appointment',
+    whatsapp: 'WhatsApp us',
+    contactInfoIntro:
+      'For general enquiries, please feel free to reach out to us using the method below:',
+    hotlineLabel: 'Hotline:',
+    emailLabel: 'Email address:',
+    agreement:
+      'Your use of our appointment service or communication via WhatsApp constitutes your agreement to our collection and use of personal data as outlined in our privacy policy.',
+    errorSpecialists:
+      'Oops! Something went wrong. Please close the app and try again.',
+    retry: 'Retry',
+    languageToggle: '中',
+  },
+  specialists: {
+    bannerTitle: 'Our GUM Specialists',
+  },
+};
